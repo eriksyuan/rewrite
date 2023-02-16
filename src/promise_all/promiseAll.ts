f7e8c80d002ce80f5be03ba484064fd9dbd9d2e1
eriksyuan <eriksyuan94@outlook.com>
2023-02-16 20:26:01 +0800

    add promise.all

--- src/promise_all/promiseAll.ts
+++ src/promise_all/promiseAll.ts
@@ -1,12 +1,12 @@
 export function promiseAll(list: Promise<any>[]): Promise<any[]> {
-  const result = new Array(list.length);
+  const result = new Array(list.length).fill(null);
 
   return new Promise((resolve, reject) => {
     for (let index = 0; index < list.length; index++) {
       const p = list[index];
       p.then((res) => {
         result[index] = res;
-        if (result.every(Boolean)) {
+        if (result.every((l) => l !== null)) {
           resolve(result);
         }
       }).catch(reject);
